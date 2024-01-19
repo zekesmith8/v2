1e9d171c30f9401cf340fb4a06ae8fd3d3c20dc8
Zeke <zekesmith8@gmail.com>
2024-01-19 15:54:46 -0800

    cleaing up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,15 +7,7 @@ function App() {
   return (
     <>
     <section>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>ByEzekiel</h1>
+      <h1 className="header">ByEzekiel</h1>
       <p>
         Software Developer
       </p>
