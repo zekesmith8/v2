a77ca09bb910191a7ffa32302f85dca854d3c395
Zeke <zekesmith8@gmail.com>
2024-01-19 15:20:42 -0800

    Little Updates to the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>ByEzekiel</h1>
+      <p>
+        Software Developer
+      </p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Welcome {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
