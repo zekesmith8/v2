ae656cc86e746189b759d2cebf7adacf9863ba29
Zeke <zekesmith8@gmail.com>
2024-01-18 22:17:26 -0800

    done for the day atleast

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -6,12 +8,14 @@ function App() {
 
   return (
     <>
-    <section>
       <div>
-        
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </section>
-      <section>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -21,12 +25,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      </section>
-      <section>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </section>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   )
 }
