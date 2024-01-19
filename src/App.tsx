01f146ca9b0d181868c010eade0f2f702e9d1906
Zeke <zekesmith8@gmail.com>
2024-01-19 15:34:07 -0800

    sprucing up the place

--- src/App.tsx
+++ src/App.tsx
@@ -25,13 +25,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           Welcome {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       </section>
       <section></section>
       <section></section>
