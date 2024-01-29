47a687bc1e63294fd30f20c217138d5c62d19401
Zeke <zekesmith8@gmail.com>
2024-01-29 11:34:52 -0800

    getting a bit more familiar with framer motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,22 +1,21 @@
 import { useState } from 'react'
+import { motion } from "framer-motion";
 import './App.css'
-import './components/Hero'
 
-function App() {
+function Hero() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <section>
-      <></>
       <h1 className="header">ByEzekiel</h1>
       <p>
         Software Developer
       </p>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <motion.button onClick={() => setCount((count) => count + 1)}>
           Oh my god a button - look at it go~! {count}
-        </button>
+        </motion.button>
       </div>
       </section>
       <section></section>
@@ -26,4 +25,4 @@ function App() {
   )
 }
 
-export default App
+export default Hero
